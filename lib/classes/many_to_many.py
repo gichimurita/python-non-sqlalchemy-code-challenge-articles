fda9c50b9d4bc92f3d9bf023500850db179b1144
Rita Gichimu <wangeci.gichimu@gmail.com>
2024-07-09 23:51:34 +0300

    finished challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,91 +1,97 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
-        self._author = author
-        self._magazine = magazine
-        self._title = title
-        author.articles().append(self)
-        magazine.articles().append(self)
+        self.author = author
+        self.magazine = magazine
+        self.title = title
+        Article.all.append(self)
 
     @property
     def title(self):
         return self._title
 
-    @property
-    def author(self):
-        return self._author
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    def __repr__(self):
-        return f"Article: {self.title} ({self.magazine.name})"
+    @title.setter
+    def title(self, title):
+        if not hasattr(self, '_title'):
+            if isinstance(title, str) and 5 <= len(title) <= 50:
+                self._title = title
+            else:
+                raise ValueError("Title must be a string between 5 and 50 characters.")
+        else:
+            raise AttributeError("Title cannot be changed once set.")
 
 class Author:
     def __init__(self, name):
-        self._name = name
-        self._articles = []
+        self.name = name
 
     @property
     def name(self):
         return self._name
+
+    @name.setter
+    def name(self, name):
+        if not hasattr(self, '_name'):
+            if isinstance(name, str) and len(name) > 0:
+                self._name = name
+            else:
+                raise ValueError("Name must be a non-empty string.")
+        else:
+            raise AttributeError("Name cannot be changed once set.")
 
     def articles(self):
-        return self._articles
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        return list(set([article.magazine for article in self._articles]))
+        return list({article.magazine for article in self.articles()})
 
     def add_article(self, magazine, title):
-        new_article = Article(self, magazine, title)
-        self._articles.append(new_article)
-        return new_article
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        magazines = self.magazines()
-        if not magazines:
-            return None
-        return list(set([magazine.category for magazine in magazines]))
-
-    def __repr__(self):
-        return f"Author: {self.name}"
-
+        return list({magazine.category for magazine in self.magazines()}) or None
+    
 class Magazine:
     def __init__(self, name, category):
-        self._name = name
-        self._category = category
-        self._articles = []
+        self.name = name  # Use the setter to set the name
+        self.category = category  # Use the setter to set the category
 
     @property
     def name(self):
         return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 2 <= len(name) <= 16:
+            self._name = name
+        else:
+            raise ValueError
 
     @property
     def category(self):
         return self._category
 
+    @category.setter
+    def category(self, category):
+        if isinstance(category, str) and len(category) > 0:
+            self._category = category
+        else:
+            raise ValueError("Category must be a non-empty string.")
+
     def articles(self):
-        return self._articles
-
-    def add_article(self, author, title):
-        new_article = Article(author, self, title)
-        self._articles.append(new_article)
-        return new_article
-
-    def article_titles(self):
-        if not self._articles:
-            return None
-        return [article.title for article in self._articles]
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        author_counts = {}
-        for article in self._articles:
-            author = article.author
-            if author in author_counts:
-                author_counts[author] += 1
-            else:
-                author_counts[author] = 1
-        return [author for author, count in author_counts.items() if count > 2]
+        return list({article.author for article in self.articles()})
 
-    def __repr__(self):
-        return f"Magazine: {self.name}, Category: {self.category}"
+    def article_titles(self):
+        return [article.title for article in self.articles()] or None
+
+    def contributing_authors(self):
+        author_articles_count = {}
+        for article in self.articles():
+            if article.author in author_articles_count:
+                author_articles_count[article.author] += 1
+            else:
+                author_articles_count[article.author] = 1
+        return [author for author, count in author_articles_count.items() if count > 2] or None
